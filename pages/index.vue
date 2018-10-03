ba270879627cd4b682e4d05bc81fb0f60c1de844
BadboYBG <minhnv0110@gmail.com>
2018-10-03 10:20:54 +0700

    nested

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,75 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center>
-    <v-flex
-      xs12
-      sm8
-      md6>
-      <div class="text-xs-center">
-        <logo/>
-        <vuetify-logo/>
-      </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a
-            href="https://vuetifyjs.com"
-            target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a
-            href="https://chat.vuetifyjs.com/"
-            target="_blank"
-            title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a
-            href="https://github.com/vuetifyjs/vuetify/issues"
-            target="_blank"
-            title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank">Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <div class="container">
+    <div class="left">
+      <h2><nuxt-link to="/">Players</nuxt-link></h2>
+      <ul class="players">
+        <li 
+          v-for="user in users" 
+          :key="user.id">
+          <nuxt-link :to="'/'+user.id">{{ user.name }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+    <div class="right">
+      <nuxt-child :key="$route.params.id" />
+    </div>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  asyncData({ env }) {
+    return { users: env.users }
   }
 }
 </script>
+
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.4s, transform 0.4s;
+  transform-style: preserve-3d;
+  backface-visibility: hidden;
+  opacity: 1;
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0.5;
+  transform: rotateY(100deg);
+}
+.container {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  box-sizing: border-box;
+}
+.left {
+  width: 50%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.right {
+  width: 50%;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.players {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.players li a {
+  display: block;
+  border: 1px #ddd solid;
+  padding: 10px;
+  text-align: left;
+  color: #222;
+  text-decoration: none;
+}
+.players li a:hover {
+  color: orange;
+}
+</style>
